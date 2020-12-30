75b3a9b24338d58eafe5f8c640b3f8bdec374b4d
anhlt <42168740+tuananhlai@users.noreply.github.com>
2020-12-30 15:44:40 +0700

    add lox parser

--- com/craftint/lox/Lox.java
+++ com/craftint/lox/Lox.java
@@ -56,6 +56,15 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        if (hadError) {
+            return;
+        }
+
+        System.out.println(new AstPrinter().print(expression));
+
         for (Token token : tokens) {
             System.out.println(token);
         }
@@ -68,5 +77,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
